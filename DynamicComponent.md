b04b45f7272b0099aef5696574f7bc6db2020ad3
ThuNX117 <thunx@mor.com.vn>
2022-08-16 16:19:38 +0700

    add intern exercise

---
+++ DynamicComponent.md
@@ -0,0 +1,21 @@
+
+# 004:  Dynamic component  
+
+## Giới thiệu
+
+Bài tập giúp các bạn làm quen dynamic Component, hiểu rõ hơn về vòng đời của vue, 
+
+![dynamicComponent](./assets/dynamicComponent.png)
+
+## Thực hiện
+
+Link Design: <https://www.figma.com/file/50HH6rGamqunPOLniC37PK/DragAndDrop?node-id=6%3A494>
+
+### Yêu cầu
+
+- [ ] Khởi tạo UI/UX giống với link figma
+- [ ] thực hiện chuyển đổi kiểu input theo option được chọn ( dạng text, dạng checkbox, dạng radio... )
+- [ ] sử dụng keep-Alive với dynamic component 
+- [ ] sử dụng emit event, v-bind, props and callback, event Bus cho dynamic component
+- [ ] Viết unit test cho component (Chức năng thêm - Sẽ thực hiện vào tuần cuối)
+- [ ] Đóng gói được component để có thể sử dụng cho App (hoặc có thể build thành thư viện và đưa lên NPM  -  Option)
---
+++ assets/bai_tap_4/data.js
@@ -0,0 +1,88 @@
+export const data = [
+  { id: "0", name: "Phòng ban 1", level: 1, code: "DPM0-0", children: [] },
+  {
+    id: "1",
+    name: "Phòng ban 1",
+    level: 1,
+    code: "DPM0-1",
+    children: [
+      {
+        id: "11",
+        name: "Phòng ban 11",
+        level: 2,
+        code: "DPM0-11",
+        children: [],
+      },
+      {
+        id: "12",
+        name: "Phòng ban 12",
+        level: 2,
+        code: "DPM0-12",
+        children: [],
+      },
+      {
+        id: "13",
+        name: "Phòng ban 13",
+        level: 2,
+        code: "DPM0-13",
+        children: [],
+      },
+      {
+        id: "14",
+        name: "Phòng ban 14",
+        level: 2,
+        code: "DPM0-14",
+        children: [
+          {
+            id: "111",
+            name: "Phòng ban 19",
+            level: 3,
+            code: "DPM0-111",
+            children: [
+              {
+                id: "1111",
+                name: "Phòng ban 111",
+                level: 4,
+                code: "DPM0-1111",
+                children: [],
+              },
+              {
+                id: "1112",
+                name: "Phòng ban 121",
+                level: 4,
+                code: "DPM0-1112",
+                children: [],
+              },
+              {
+                id: "1113",
+                name: "Phòng ban 131",
+                level: 4,
+                code: "DPM0-1113",
+                children: [],
+              },
+            ],
+          },
+          {
+            id: "112",
+            name: "Phòng ban A",
+            level: 3,
+            code: "DPM0-112",
+            children: [],
+          },
+          {
+            id: "113",
+            name: "Phòng ban B",
+            level: 3,
+            code: "DPM0-113",
+            children: [],
+          },
+        ],
+      },
+    ],
+  },
+  { id: "7", name: "Phòng ban 1A", level: 1, code: "DPM0-7", children: [] },
+  { id: "8", name: "Phòng ban 1", level: 1, code: "DPM0-8", children: [] },
+  { id: "9", name: "Phòng ban 1", level: 1, code: "DPM0-9", children: [] },
+  { id: "10", name: "Phòng ban 1", level: 1, code: "DPM0-10", children: [] },
+  { id: "11", name: "Phòng ban 1", level: 1, code: "DPM0-11", children: [] },
+];
